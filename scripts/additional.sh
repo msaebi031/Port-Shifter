7a13b6648ae355fa3202379402fb29a0f92505b8
mr_artan <44286111+msaebi031@users.noreply.github.com>
2025-03-01 11:46:37 +0330

    Update additional.sh

--- scripts/additional.sh
+++ scripts/additional.sh
@@ -1,25 +1,26 @@
+#!/bin/bash
+
 source /opt/Port-Shifter/scripts/path.sh
 source /opt/Port-Shifter/scripts/package.sh
 
 configure_dns() {
-
     sudo cp /etc/resolv.conf /etc/resolv.conf.backup
     sudo rm /etc/resolv.conf > /dev/null 2>&1
 
-    dns1=$(whiptail --inputbox "Enter DNS Server 1 (like 8.8.8.8):" 8 60 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    
-    if [ $exitstatus != 0 ] || [ -z "$dns1" ]; then
-        whiptail --title "DNS Configuration" --msgbox "Operation cancelled or invalid input. Restoring default DNS configuration." 8 60
+    echo "Enter DNS Server 1 (like 8.8.8.8):"
+    read dns1
+
+    if [ -z "$dns1" ]; then
+        echo "Operation cancelled or invalid input. Restoring default DNS configuration."
         restore_dns
         exit 1
     fi
 
-    dns2=$(whiptail --inputbox "Enter DNS Server 2 (like 8.8.4.4):" 8 60 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    
-    if [ $exitstatus != 0 ] || [ -z "$dns2" ]; then
-        whiptail --title "DNS Configuration" --msgbox "Operation cancelled or invalid input. Restoring default DNS configuration." 8 60
+    echo "Enter DNS Server 2 (like 8.8.4.4):"
+    read dns2
+
+    if [ -z "$dns2" ]; then
+        echo "Operation cancelled or invalid input. Restoring default DNS configuration."
         restore_dns
         exit 1
     fi
@@ -27,7 +28,7 @@ configure_dns() {
     echo "nameserver $dns1" | sudo tee -a /etc/resolv.conf > /dev/null
     echo "nameserver $dns2" | sudo tee -a /etc/resolv.conf > /dev/null
 
-    whiptail --title "DNS Configuration" --msgbox "DNS Configuration completed." 8 60
+    echo "DNS Configuration completed."
     clear
 }
 
@@ -36,45 +37,40 @@ restore_dns() {
     echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf > /dev/null
 }
 
-function update_server() {
-    (
-        sudo $PACKAGE_MANAGER update -y
-        echo "100" "Update completed."
-    ) | dialog --title "Update Server" --progressbox 30 120
-
-    whiptail --title "Update Server" --msgbox "Server Update completed." 8 60
+update_server() {
+    echo "Updating server..."
+    sudo $PACKAGE_MANAGER update -y
+    echo "Update completed."
     clear
 }
 
-function ping_websites() {
+ping_websites() {
     websites=("github.com" "google.com" "www.cloudflare.com")
     results_file=$(mktemp)
 
     for website in "${websites[@]}"; do
-        gauge_title="Pinging $website"
-        gauge_percentage=0
+        echo "Pinging $website..."
         success=false
 
-        (
-            for _ in {1..5}; do
-                sleep 1  
-                ((gauge_percentage += 20))
-                echo "$gauge_percentage"
-                echo "# $gauge_title"
-                echo "Pinging $website..."
-                
-                if ping -c 1 $website &> /dev/null; then
-                    success=true
-                fi
-            done
-            echo "100" 
-        ) | dialog --title "Ping $website" --gauge "$gauge_title" 10 80 0
+        for _ in {1..5}; do
+            sleep 1  
+            echo "Pinging $website..."
+            if ping -c 1 $website &> /dev/null; then
+                success=true
+                break
+            fi
+        done
 
-        result=$(ping -c 5 $website | tail -n 2)
-        echo -e "\n\nPing results for $website:\n$result" >> "$results_file"
+        if $success; then
+            result="Ping successful"
+        else
+            result="Ping failed"
+        fi
+
+        echo -e "\nPing results for $website: $result" >> "$results_file"
     done
 
-    whiptail --title "Ping Websites" --textbox "$results_file" 30 80
+    cat "$results_file"
     clear
 
     rm "$results_file"
